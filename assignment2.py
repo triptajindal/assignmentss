384067ab8e14f383493b6fcdeb097b50fa2ea406
tripss <32153500+triptajindal@users.noreply.github.com>
2018-12-28 20:10:33 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,28 @@
+#print anything
+print('hello world')
+
+#join 2 string
+print("tripta "+"jindal")
+
+#take input nd print
+a=input("enter first value- ")
+b=input("enter second value- ")
+c=input("enter third value- ")
+print(a)
+print(b)
+print(c)
+
+#print a line
+print("Let's get started")
+
+#using placeholder
+s="Acadview"
+course="python"
+fees=5000
+print("{} institute offer a course of {} in Rs {}".format(s,course,fees))
+
+#area of circle
+r=int(input("enter the radius"))
+area=3.14*r*r
+print(area)
+
